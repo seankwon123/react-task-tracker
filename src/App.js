90cb6672935cc6df8b105f28a82246c1f7ae9bdf
kwonbada <kwonbada@umich.edu>
2021-07-03 13:45:44 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello From React!</h1>
+      <h2>Dear god a Task Tracker?</h2>
+
+      <Header />
+
     </div>
-  );
+  )
 }
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a class!</h1>
+//   }
+// }
 
 export default App;
